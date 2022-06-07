814ceb7242bd9e74992a7d36563160f741e52b12
David Puerto <dsp5502@gmail.com>
2022-06-07 09:23:10 -0500

    Agregando un botón para eliminar elementos del carrito

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/normalize.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   const [carrito, setCarrito] = useState([]);
+
+  useEffect(() => {
+    const carritoLS = JSON.parse(localStorage.getItem('carrito')) ?? [];
+
+    if (carritoLS.length !== 0) {
+      setCarrito(carritoLS);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+  }, [carrito]);
+
   const agregarCarrito = (producto) => {
     if (carrito.some((articulo) => articulo.id === producto.id)) {
       const carritoActualizado = carrito.map((articulo) => {
@@ -17,11 +30,29 @@ function MyApp({ Component, pageProps }) {
       setCarrito([...carrito, producto]);
     }
   };
+
+  const actualizarCantidad = (producto) => {
+    const carritoActualizado = carrito.map((articulo) => {
+      if (articulo.id === producto.id) {
+        articulo.cantidad = producto.cantidad;
+      }
+      return articulo;
+    });
+    setCarrito(carritoActualizado);
+  };
+
+  const eliminarProducto = (id) => {
+    const carritoActualizado = carrito.filter((articulo) => articulo.id !== id);
+    setCarrito(carritoActualizado);
+  };
+
   return (
     <Component
       {...pageProps}
       carrito={carrito}
       agregarCarrito={agregarCarrito}
+      actualizarCantidad={actualizarCantidad}
+      eliminarProducto={eliminarProducto}
     />
   );
 }
--- pages/carrito.js
+++ pages/carrito.js
@@ -1,12 +1,68 @@
+import Image from 'next/image';
 import Layout from '../components/Layout';
 import styles from '../styles/carrito.module.css';
 
-const Carrito = ({ carrito }) => {
+const Carrito = ({ carrito, actualizarCantidad, eliminarProducto }) => {
   return (
     <Layout pagina={'Carrito de Compras'}>
       <h1 className='heading'>Carrito</h1>
       <main className={` ${styles.contenido} contenedor`}>
-        <div>1</div>
+        <div className={` ${styles.carrito}`}>
+          {carrito.length === 0
+            ? 'Carrito Vacio'
+            : carrito.map((articulo) => (
+                <div key={articulo.id} className={styles.producto}>
+                  <div>
+                    <Image
+                      layout='responsive'
+                      src={articulo.imagen}
+                      alt={`Imagen ${articulo.nombre}`}
+                      width={250}
+                      height={500}
+                    />
+                  </div>
+                  <div>
+                    <p className={styles.nombre}>{articulo.nombre}</p>
+                    <div className={styles.cantidad}>
+                      <p>Cantidad:</p>
+                      <select
+                        className={styles.select}
+                        value={articulo.cantidad}
+                        id='cantidad'
+                        onChange={(e) =>
+                          actualizarCantidad({
+                            cantidad: e.target.value,
+                            id: articulo.id,
+                          })
+                        }
+                      >
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                        <option value='4'>4</option>
+                        <option value='5'>5</option>
+                        <option value='6'>6</option>
+                        <option value='7'>7</option>
+                      </select>
+                    </div>
+                    <p className={styles.precio}>
+                      $ <span>{articulo.precio}</span>
+                    </p>
+                    <p className={styles.subtotal}>
+                      Subtotal: $
+                      <span>{articulo.precio * articulo.cantidad}</span>
+                    </p>
+                  </div>
+                  <button
+                    onClick={() => eliminarProducto(articulo.id)}
+                    className={styles.eliminar}
+                    type='button'
+                  >
+                    X
+                  </button>
+                </div>
+              ))}
+        </div>
         <div>2</div>
       </main>
     </Layout>
